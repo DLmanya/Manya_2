89311d39d59c4c857f037a4c489af3bbf511a297
Manya <manjak29@yandex.ru>
2022-11-25 20:47:57 +0400

    задача 25

---
+++ work_1/Program.cs
@@ -0,0 +1,15 @@
+﻿int GetExponentiation(int NumberA, int NumberB)
+{
+  int result = 1;
+  for(int i = 1; i <= NumberB; i++)
+  {
+    result = result * NumberA;
+  }
+  return result;
+}
+Console.Write("Введите число A: ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+int Exponentiation = GetExponentiation(NumberA, NumberB);
+Console.WriteLine("Ответ: " + Exponentiation);
